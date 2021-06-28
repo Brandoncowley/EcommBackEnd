3dfc821f424b8895f8c4ad1a17060d1d5c3337f7
Brandon Cowley <brandon.cowley@gmail.com>
2021-06-27 18:03:30 -0700

    product routes are working

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findAll( {
-      include: [{ model: Product, through: ProductTag, as: 'tags_products' }]
+      include: [{ model: Product, as: 'tag_product' }]
     });
 
     if (!tagData) {
@@ -27,7 +27,7 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product, through: ProductTag, as: 'tags_products' }]
+      include: [{ model: Product, as: 'tag_product' }]
     });
 
     if (!tagData) {
